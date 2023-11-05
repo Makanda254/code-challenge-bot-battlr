860b8e778e4f486fb7e4092615f8e6a992487fef
Victor <victormakanda254@gmail.com>
2023-11-06 00:36:10 +0300

    Function to release bot from army

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,13 +1,19 @@
 import React from 'react';
 //import Bot from './Bot';
 
-export default function YourBotArmy({botArmy}) {
-  console.log(botArmy)
+export default function YourBotArmy({botArmy, setBotArmy}) {
+  //console.log(botArmy)
+  function handleRelease(id){
+    const upDatedBotArmy = botArmy.filter((bot) => bot.id !== id)
+    setBotArmy(upDatedBotArmy);
+  }
+  
   return (
     <div>
       {
         botArmy.map((itemBot) => (
-          <div>
+          <div onClick={() => handleRelease(itemBot.id)}>
+            <h1>Your Bot Army</h1>
            <img src={itemBot.avatar_url} alt="A Bot"/>
            <h3>{itemBot.name}</h3>
            <p>{itemBot.catchphrase}</p>
